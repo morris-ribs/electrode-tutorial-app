a6ffc08af0041bd78c8abe3175ed61cb0d7096da
Mauricio Ribeiro <maumribeiro@gmail.com>
2016-11-17 16:22:03 +0100

    Integrating the reusable component

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import {Route} from "react-router";
+import {IndexRoute, Route} from "react-router";
 import Home from "./components/home";
+import { FirstReusableComponent } from "first-reusable-component";
 
 export const routes = (
-  <Route path="/" component={Home}/>
+  <Route path="/" component={Home}>
+    <IndexRoute component={FirstReusableComponent}/>
+    <Route path="/invite" component={FirstReusableComponent}/>
+  </Route>
 );
